a0a7fb5e6ee2e60f0e958a4fc3e68676b34e699b
mstree <111582436+mysocialtree@users.noreply.github.com>
2022-08-20 02:36:03 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,8 @@
-
+// url https://mstree.de/api/get.php + ? + api=token
+fetch('https://mstree.de/api/get.php?api=token') //get data from server 
+    .then(result => result.json()) // convert data to json 
+    .then((output) => {   // output data
+  
+        console.log(output["id"]);  // print data to consol 
+  
+    }).catch(err => console.error(err)); // catch error 
